69933903f8e9ae2641c74febb4288d09b4757080
exersalza <squidodle44@gmail.com>
2022-10-01 14:53:27 +0200

    Changed the regex a bit, to prevent single use of \xXX hex chars and added verbose method for command line information about finders.

--- cipherFinder/finder.py
+++ cipherFinder/finder.py
@@ -1,4 +1,4 @@
-#!/bin/python3.10
+#!/bin/python3.8
 
 #  Copyright (c) 2022. - exersalza
 #
@@ -26,7 +26,7 @@ import re
 import sys
 from datetime import datetime as dt
 
-REGEX = r'((\\x)([a-fA-F0-9]{2}))'
+REGEX = r'(((\\x)([a-fA-F0-9]{2})){2})'
 
 
 def validate_lines(lines) -> list[tuple]:
@@ -35,12 +35,12 @@ def validate_lines(lines) -> list[tuple]:
     Parameters
     ----------
     lines : list
-        The lines from the current readed file.
+        The lines from the current read file.
 
     Returns
     -------
     list[tuple]
-        A list with infected lines (or false positives by Anticheat or obfuscated code)
+        A list with infected lines (or false positives by AntiCheat or obfuscated code)
     """
 
     ret = []
@@ -57,8 +57,13 @@ def main() -> int:
 
     Usage:
     ------
-    Run the program: `python3.10 <path>`. path = give your fullpath to the files you want to scan, otherwise it will
-    use the `.` as path (the current working directory).
+    Run the program: `find-cipher <path> [exclude path] [--verbose]`.
+
+    args:
+        path : Give the path to search, when no path is given, the current working directory will be used `.` .
+        exclude path : Optional : Exclude directory's where you don't want to search.
+        --verbose : Optional : Print a Cipher directly to the Command line on found.
+
 
     Advertisement:
     --------------
@@ -77,7 +82,7 @@ def main() -> int:
 
     log = []
     count = 0
-    pattern = ''.join([i.replace(',', ')|(') for i in sys.argv[2:]])
+    pattern = ''.join([(i.replace(',', ')|(') if '--' not in i else '') for i in sys.argv[2:]])
 
     for d, _, files in os.walk(sys.argv[1] if len(sys.argv) > 1 else '.'):
         if pattern and re.findall(f'{"(" + pattern + ")"}', fr'{d}'.format(d=d), re.MULTILINE and re.IGNORECASE):
@@ -91,6 +96,7 @@ def main() -> int:
                 try:
                     lines = f.readlines()
                 except UnicodeDecodeError:
+                    print(f'Can\'t decode `{d}/{file}`.')
                     continue
 
                 match = validate_lines(lines)
@@ -98,7 +104,12 @@ def main() -> int:
                 if match:
                     for ln, line in match:
                         path = d.replace('\\', '/') + f'/{file}'
-                        log.append(f'File: {path}\nLineNumber: {ln}\nLine: \'{line}\'\n----------------\n')
+                        to_log = f'File: {path}\nLineNumber: {ln}\n'
+
+                        if '--verbose' in sys.argv:  # Log in console.
+                            print(to_log)
+
+                        log.append(to_log + f'Line: \'{line}\'\n----------------\n')
                         count += 1
 
     # Write log
