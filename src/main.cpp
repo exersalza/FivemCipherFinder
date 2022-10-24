76091456ee6fa6218f99edbd2101254b2247bf29
exersalza <squidodle44@gmail.com>
2022-10-24 15:50:42 +0200

    added test file and regex inside the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,33 @@
 //
 // Created by julian on 10/24/22.
 //
+
+#include <iostream>
+#include <fstream>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    string regex = R"((((\x|\u)([a-fA-F0-9]{2})){2}))";
+
+    fstream file;
+    string line;
+    string foundCipher;
+    int count = 0;
+
+    file.open("../src/testFile.lua", ios::in | ios::app);
+    if (!file.is_open()) { file.close(); return 1;}
+
+    while (!file.eof()) {
+        count++;
+        getline(file, line, '\n');
+        if (line.empty())
+            continue;
+    }
+    file.close();
+    cout << count << endl;
+
+
+    return 0;
+}
---
+++ src/testFile.lua
@@ -0,0 +1,8 @@
+function testinStuff()
+    print("testinStuff")
+end
+
+f = ['\x24', '\x25', '\x26', '\x27']
+
+
+f = ['\x24', '\x25', '\x26', '\x27']
