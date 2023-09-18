6a343317347e6e1be603ec531577759510a6352f
exersalza <squidodle44@gmail.com>
2023-09-18 14:17:33 +0200

    added tests

--- tests/test_deleter.py
+++ tests/test_deleter.py
@@ -1,2 +1,11 @@
+from cipherFinder.deleter import y_n_validator
+
+def test_y_n_validator():
+    assert y_n_validator("y")
+    assert y_n_validator("yes")
+    
+    assert not y_n_validator("jfkdslajflk")
+
+
 def test_deleter_main():  # yeah, naaahhh
     ...
