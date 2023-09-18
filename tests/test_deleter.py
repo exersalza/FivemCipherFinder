4bfe7bd23bc256954815e3b72d55bd325dfd9a66
exersalza <squidodle44@gmail.com>
2023-09-18 14:19:51 +0200

    fix linters AGAIN

--- tests/test_deleter.py
+++ tests/test_deleter.py
@@ -1,9 +1,10 @@
 from cipherFinder.deleter import y_n_validator
 
+
 def test_y_n_validator():
     assert y_n_validator("y")
     assert y_n_validator("yes")
-    
+
     assert not y_n_validator("jfkdslajflk")
 
 
