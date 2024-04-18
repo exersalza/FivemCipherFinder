a633d1795b66f43aea7676a1aea705b9dff336fb
exersalza (Julian J.) <squidodle44@gmail.com>
2024-04-18 09:17:24 +0200

    e

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -17,6 +17,18 @@ class Init(PluginInterface):
         return "dummy result"
                 """
             )
+            
+        with open(os.path.join(tmpdir, "test_plugin.py"), "w", encoding="utf-8") as f:
+            f.write(
+                """
+from cipherFinder.plugins import PluginInterface
+
+class FetchMeTheirSouls(PluginInterface):
+    hook_name = "GetFileContents"
+    def execute(self, *args, **kw):
+        return "dummy result"               
+                """
+            )
 
         # Load the plugins from the temporary directory
         plugins = load_plugs(tmpdir)
@@ -24,3 +36,7 @@ class Init(PluginInterface):
         # Check that the DummyPlugin was loaded and returns the expected result
         assert "Init" in plugins
         assert plugins["Init"].execute() == "dummy result"
+        
+        # check if you can actually use "hook_name" as class name
+        assert "GetFileContents" in plugins
+        assert plugins["GetFileContents"].execute() == "dummy result"
